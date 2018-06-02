0ba3355f4f3e3ea3c405a5159bcf3a04369963d8
techno-tanoC <develop.tanoc@gmail.com>
2018-06-02 15:49:59 +0900

    commit

--- setup.sh
+++ setup.sh
@@ -35,6 +35,26 @@ docker () {
   sudo systemctl start docker.service
 }
 
+git () {
+  echo "install and config git"
+
+  name="techno-tanoC"
+  email="develop.tanoc@gmail.com"
+
+  sudo pacman -S --noconfirm git tig
+
+  git config --global user.name "$name"
+  git config --global user.email "$email"
+  git config --global core.editor vim
+  git config --global merge.tool vimdiff
+  git config --global push.default current
+  git config --global alias.ignore '!gi() { curl -L -s https://www.gitignore.io/api/$@ ;}; gi'
+
+  mkdir ~/.ssh
+  ssh-keygen -t rsa -C "$email" -N "" -f "$HOME/.ssh/id_rsa"
+
+}
+
 base
 app
 docker
@@ -42,3 +62,6 @@ docker
 LANG=C xdg-user-dirs-gtk-update
 
 echo -e "\e[32mPlease log out. You may be necessary to reboot\e[m"
+echo
+echo "register ssh key:"
+echo -e "\e[32m$(cat "$HOME/.ssh/id_rsa.pub")\e[m"
