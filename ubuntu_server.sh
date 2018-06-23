51775721535c82b649aab704e4ce815dec82d459
techno-tanoC <develop.tanoc@gmail.com>
2018-06-23 15:35:25 +0000

    add ubuntu_server.sh

---
+++ ubuntu_server.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_git () {
+  echo "install and config git"
+
+  name="techno-tanoC"
+  email="develop.tanoc@gmail.com"
+
+  sudo pacman -S --refresh --noconfirm git tig
+
+  git config --global user.name "$name"
+  git config --global user.email "$email"
+  git config --global core.editor vim
+  git config --global merge.tool vimdiff
+  git config --global push.default current
+  git config --global alias.ignore '!gi() { curl -L -s https://www.gitignore.io/api/$@ ;}; gi'
+
+  mkdir ~/.ssh
+  ssh-keygen -t rsa -C "$email" -N "" -f "$HOME/.ssh/id_rsa"
+}
