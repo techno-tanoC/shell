0abd8660ae8f5c9693c45be8db90563d55500466
techno-tanoC <develop.tanoc@gmail.com>
2018-08-11 16:41:30 +0900

    update ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -33,6 +33,12 @@ setup_vscode() {
   wget https://az764295.vo.msecnd.net/stable/1dfc5e557209371715f655691b1235b6b26a06be/code_1.25.1-1531323788_amd64.deb
   sudo dpkg -i code_1.25.1-1531323788_amd64.deb
   rm code_1.25.1-1531323788_amd64.deb
+
+  code --install-extension ms-ceintl.vscode-language-pack-ja
+  code --install-extension vscodevim.vim
+  code --install-extension justusadam.language-haskell
+  code --install-extension rust-lang.rust
+  code --install-extension mjmcloug.vscode-elixir
 }
 
 setup_fcitx () {
