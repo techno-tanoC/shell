13d376dec5b01b0439ecba0d8445ac74a6f4e158
techno-tanoC <develop.tanoc@gmail.com>
2019-01-26 01:34:20 +0900

    update code

--- code.sh
+++ code.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+# common
 code --install-extension ms-ceintl.vscode-language-pack-ja
-code --install-extension vscodevim.vim
-code --install-extension justusadam.language-haskell
-code --install-extension rust-lang.rust
+code --install-extension pkief.material-icon-theme
+
+# code --install-extension vscodevim.vim
+
+# ruby
+code --install-extension rebornix.ruby
+
+# elixir
 code --install-extension mjmcloug.vscode-elixir
+
+# rust
+code --install-extension rust-lang.rust
+
+# elm
+code --install-extension sbrink.elm
+code --install-extension necinc.elmmet
+
+# haskell
+# code --install-extension justusadam.language-haskell
