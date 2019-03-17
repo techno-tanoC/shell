d07bf84e4df870108563f16814f7e73746e3a1cc
techno-tanoC <develop.tanoc@gmail.com>
2019-03-17 18:23:56 +0900

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -37,7 +37,7 @@ setup_docker () {
   curl -fsSL get.docker.com | sh -s
   sudo usermod -aG docker $USER
 
-  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 }
 
