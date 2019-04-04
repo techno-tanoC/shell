c903ac42496ec13e28680605d6ffa5086424acae
techno-tanoC <develop.tanoc@gmail.com>
2019-04-04 21:52:24 +0900

    add elm to lang.sh

--- lang.sh
+++ lang.sh
@@ -24,6 +24,7 @@ setup_asdf() {
   bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
   asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
   asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+  asdf plugin-add elm https://github.com/vic/asdf-elm.git
 }
 
 setup_ruby() {
