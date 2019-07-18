73979f3f494549bfb72af89b9b3a10cfb9a6f1ce
techno-tanoC <develop.tanoc@gmail.com>
2019-07-18 17:16:44 +0900

    add kube

---
+++ kube.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+if [ `uname` = "Darwin" ] ; then
+  # brew cask install google-cloud-sdk
+  # brew install kubectx
+  # brew install kubernetes-helm
+elif [ `lsb_release -is` = "ManjaroLinux" ] ; then
+  curl https://sdk.cloud.google.com | bash
+  exec -l $SHELL
+  gcloud init
+  gcloud components install kubectl
+  sudo pacman -S --noconfirm kubectx
+  yay -S --noconfirm kubernetes-helm
+elif [ `lsb_release -is` = "Ubuntu" ] ; then
+  curl https://sdk.cloud.google.com | bash
+  exec -l $SHELL
+  gcloud init
+  gcloud components install kubectl
+  sudo apt install -y kubectx
+else
+  exit 1
+fi
+
+# gcloud auth login
+
+# istio
