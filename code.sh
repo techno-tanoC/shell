1b665079e10b8e2f923071261c37f10ab26b0cc9
techno-tanoC <develop.tanoc@gmail.com>
2019-10-31 18:51:38 +0900

    update code.sh

--- code.sh
+++ code.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
+# code --list-extensions
+
 # common
 code --install-extension ms-ceintl.vscode-language-pack-ja
 code --install-extension pkief.material-icon-theme
 code --install-extension nonylene.dark-molokai-theme
 code --install-extension vscodevim.vim
-code --install-extension atishay-jain.all-autocomplete
-
-# docker
-code --install-extension ms-azuretools.vscode-docker
-
-# terraform
-code --install-extension mauve.terraform
+code --install-extension TabNine.tabnine-vscode
+# code --install-extension atishay-jain.all-autocomplete
 
 # ruby
 code --install-extension rebornix.ruby
+code --install-extension castwide.solargraph
 
 # elixir
 code --install-extension jakebecker.elixir-ls
@@ -28,3 +26,7 @@ code --install-extension necinc.elmmet
 
 # haskell
 # code --install-extension justusadam.language-haskell
+
+# devops
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension mauve.terraform
