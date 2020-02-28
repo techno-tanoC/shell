78953c86118c90c0c1bc3f659e684ace551ae959
techno-tanoC <develop.tanoc@gmail.com>
2020-02-29 02:43:02 +0900

    add rust extentions

--- code.sh
+++ code.sh
@@ -19,6 +19,8 @@ code --install-extension jakebecker.elixir-ls
 
 # rust
 code --install-extension rust-lang.rust
+code --install-extension serayuzgur.crates
+code --install-extension bungcip.better-toml
 
 # elm
 code --install-extension sbrink.elm
