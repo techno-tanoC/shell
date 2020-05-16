3aad5a10405d3933ab5c2075d4d4b1afd3e62420
techno-tanoC <develop.tanoc@gmail.com>
2020-05-16 15:03:30 +0900

    update asdf

--- asdf.sh
+++ asdf.sh
@@ -6,24 +6,6 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 . $HOME/.asdf/asdf.sh
 set -e
 
-# ruby
-# refer below:
-# https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-if [ `uname` = "Darwin" ] ; then
-  brew install openssl readline
-elif [ `lsb_release -is` = "ManjaroLinux" ] ; then
-  sudo pacman -S --noconfirm --needed base-devel libffi libyaml openssl zlib
-  sudo pacman -S --noconfirm --needed sqlite
-elif [ `lsb_release -is` = "Ubuntu" ] ; then
-  sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
-  sudo apt install -y libsqlite3-dev
-else
-  echo "unsupported OS"
-  exit 1
-fi
-
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-
 # erlang and elixir
 # refer below:
 # https://github.com/asdf-vm/asdf-erlang#before-asdf-install
