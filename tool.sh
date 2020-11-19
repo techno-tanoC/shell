af76b3aca232dfb5d41ebe474b7d9078dadec528
techno-tanoC <develop.tanoc@gmail.com>
2020-11-19 20:40:07 +0900

    fix tool.sh

--- tool.sh
+++ tool.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-mkdi ~/bin
+mkdir -p ~/bin
 
 curl -L https://github.com/itchyny/mmv/releases/download/v0.1.1/mmv_v0.1.1_linux_amd64.tar.gz | tar -zxf -
 mv mmv_v0.1.1_linux_amd64/mmv ~/bin/mmv
