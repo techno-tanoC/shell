eaab0a00faee6930bd7c1583e35794404488fed2
techno-tanoC <develop.tanoc@gmail.com>
2020-11-19 18:27:46 +0900

    install docker with apt in ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -47,15 +47,8 @@ setup_fcitx() {
 }
 
 setup_docker() {
-  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
-  sudo apt-get install -y docker-ce
+  sudo apt install -y docker docker-compose
   sudo usermod -aG docker $USER
-
-  sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
 }
 
 setup_base
