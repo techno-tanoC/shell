6ea0388a6ab843c2c83111434223a992f1076b95
techno-tanoC <develop.tanoc@gmail.com>
2020-11-20 21:44:19 +0900

    delete tool.sh

--- tool.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-mkdir -p ~/bin
-
-curl -L https://github.com/itchyny/mmv/releases/download/v0.1.1/mmv_v0.1.1_linux_amd64.tar.gz | tar -zxf -
-mv mmv_v0.1.1_linux_amd64/mmv ~/bin/mmv
-rm -rf mmv_v0.1.1_linux_amd64
