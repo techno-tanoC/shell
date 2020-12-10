23c7e059e6b916d03502effe33f9a92bbfeae261
techno-tanoC <develop.tanoc@gmail.com>
2020-12-10 14:38:57 +0900

    update arch

--- arch.sh
+++ arch.sh
@@ -29,7 +29,7 @@ setup_app() {
   # yay -S --noconfirm google-chrome
   yay -S --noconfirm patch slack-desktop
   yay -S --noconfirm visual-studio-code-bin
-  yay -S --noconfirm bitwarden-bin
+  yay -S --noconfirm bitwarden
   yay -S --noconfirm xsel
 
   # install font
