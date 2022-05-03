bae3a564c4156d0cda167f61341c126efeed6a0d
techno-tanoC <develop.tanoc@gmail.com>
2022-05-03 21:22:05 +0900

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,14 +1,18 @@
+wsl --set-default-version 2
+
 winget install -e --id Bitwarden.Bitwarden
 winget install -e --id Discord.Discord
 winget install -e --id Mozilla.Firefox
 winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Google.Chrome
-winget install -e --id Docker.DockerDesktop
 winget install -e --id Insomnia.Insomnia
-winget install -e --id Git.Git
 winget install -e --id SlackTechnologies.Slack
 winget install -e --id Oracle.VirtualBox
 winget install -e --id Twilio.Authy
 winget install -e --id Amazon.Kindle
 winget install -e --id VideoLAN.VLC
 winget install -e --id Google.AndroidStudio
+winget install -e --id Valve.Steam
+
+rem winget install -e --id Google.Chrome
+rem winget install -e --id Docker.DockerDesktop
+rem winget install -e --id Git.Git
