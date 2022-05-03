30ebc6bb17799a7fbaed18da54560245fab39f0b
techno-tanoC <develop.tanoc@gmail.com>
2022-05-03 19:32:48 +0900

    Add windows.bat

---
+++ windows.bat
@@ -0,0 +1,14 @@
+winget install -e --id Bitwarden.Bitwarden
+winget install -e --id Discord.Discord
+winget install -e --id Mozilla.Firefox
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Google.Chrome
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Insomnia.Insomnia
+winget install -e --id Git.Git
+winget install -e --id SlackTechnologies.Slack
+winget install -e --id Oracle.VirtualBox
+winget install -e --id Twilio.Authy
+winget install -e --id Amazon.Kindle
+winget install -e --id VideoLAN.VLC
+winget install -e --id Google.AndroidStudio
