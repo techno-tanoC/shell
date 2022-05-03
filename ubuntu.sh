a65e3a4c927334e6b07f9ef3f992936367aa17a5
techno-tanoC <develop.tanoc@gmail.com>
2022-05-03 21:24:16 +0900

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -34,26 +34,8 @@ setup_fcitx() {
   # https://kinakoankon.net/ubuntu-20-04-japanese-input-ibus-fcitx-mozc/
 }
 
-setup_docker() {
-  sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-  sudo usermod -aG docker $USER
-}
-
 setup_base
 setup_app
 setup_fcitx
-setup_docker
 
 LANG=C xdg-user-dirs-gtk-update
