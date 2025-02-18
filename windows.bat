878173b7adf6ec0c4603b9f2b390f15225977022
techno-tanoC <develop.tanoc@gmail.com>
2025-02-18 22:22:18 +0900

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -5,13 +5,11 @@ winget install -e --id AgileBits.1Password
 winget install -e --id Discord.Discord
 winget install -e --id Mozilla.Firefox
 winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Insomnia.Insomnia
+rem winget install -e --id Insomnia.Insomnia
 winget install -e --id SlackTechnologies.Slack
 winget install -e --id Oracle.VirtualBox
-winget install -e --id Twilio.Authy
 winget install -e --id Amazon.Kindle
 winget install -e --id VideoLAN.VLC
-winget install -e --id Google.AndroidStudio
 winget install -e --id Valve.Steam
 winget install -e --id Microsoft.PowerToys
 
